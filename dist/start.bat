a3b36ffd7ac25e4962fa681e6389e9e273407ffa
ljalonen <lhjalonen@gmail.com>
2014-07-10 09:19:20 +0300

    Removed references to common.packages.
    Fixed Windows .bat

--- dist/start.bat
+++ dist/start.bat
@@ -1,2 +1,3 @@
 rmdir /S /Q fwdir
-java -Dfile.encoding=UTF-8 -Dorg.knopflerfish.gosg.jars=file:jars/ -jar framework.jar -xargs init.xargs -xargs props.xargs -init
+java -Dfile.encoding=UTF-8 -Dorg.knopflerfish.gosg.jars=file:jars/ -jar framework-7.0.1.jar -xargs init.xargs -xargs props.xargs -init
+PAUSE
