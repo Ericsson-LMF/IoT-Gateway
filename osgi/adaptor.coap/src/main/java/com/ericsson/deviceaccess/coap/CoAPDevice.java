f18aff780cc464645905addb23007902dcc8a050
WaDelma <delma@wa>
2014-07-15 16:04:25 +0300

    Merged Ericssons upstream and resolved problems that arised by our edits. Improved FunctionalUtils doIfCan method.

--- osgi/adaptor.coap/src/main/java/com/ericsson/deviceaccess/coap/CoAPDevice.java
+++ osgi/adaptor.coap/src/main/java/com/ericsson/deviceaccess/coap/CoAPDevice.java
@@ -1,6 +1,6 @@
 /*
  * Copyright Ericsson AB 2011-2014. All Rights Reserved.
- * 
+ *
  * The contents of this file are subject to the Lesser GNU Public License,
  *  (the "License"), either version 2.1 of the License, or
  * (at your option) any later version.; you may not use this file except in
@@ -9,12 +9,12 @@
  * retrieved online at https://www.gnu.org/licenses/lgpl.html. Moreover
  * it could also be requested from Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * 
+ *
  * BECAUSE THE LIBRARY IS LICENSED FREE OF CHARGE, THERE IS NO
  * WARRANTY FOR THE LIBRARY, TO THE EXTENT PERMITTED BY APPLICABLE LAW.
  * EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR
  * OTHER PARTIES PROVIDE THE LIBRARY "AS IS" WITHOUT WARRANTY OF ANY KIND,
- 
+
  * EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
  * PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE
@@ -29,30 +29,32 @@
  * (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING RENDERED
  * INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A FAILURE
  * OF THE LIBRARY TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF SUCH
- * HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES. 
- * 
+ * HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ *
  */
 package com.ericsson.deviceaccess.coap;
 
 /**
  * CoAPDevice
  *
- * This class represents a CoAP device inside of the adaptor. This is the class that is registered to the OSGi
- * framework thus enabling the access of the device from the GDA, because it extends the GenericDeviceImpl class.
- * This class is also the place where the service schemas are attached (via the putService() method call in 
- * the CoAPDeviceAgent class) and the the service parameter updates are handled. Service parameter updates are
- * when new data is received from the device and passed on to the GDA.
+ * This class represents a CoAP device inside of the adaptor. This is the class
+ * that is registered to the OSGi framework thus enabling the access of the
+ * device from the GDA, because it extends the GenericDeviceImpl class. This
+ * class is also the place where the service schemas are attached (via the
+ * putService() method call in the CoAPDeviceAgent class) and the the service
+ * parameter updates are handled. Service parameter updates are when new data is
+ * received from the device and passed on to the GDA.
  */
-
+import com.ericsson.commonutil.LegacyUtil;
 import com.ericsson.deviceaccess.api.Constants;
-import com.ericsson.deviceaccess.api.GenericDeviceService;
-import com.ericsson.deviceaccess.spi.schema.SchemaBasedGenericDevice;
+import com.ericsson.deviceaccess.api.genericdevice.GDService;
+import com.ericsson.deviceaccess.spi.schema.based.SBGenericDevice;
+import java.net.URI;
+import java.util.Map;
 import org.osgi.framework.ServiceRegistration;
 
-import java.net.URI;
-import java.util.Properties;
+public class CoAPDevice extends SBGenericDevice {
 
-public class CoAPDevice extends SchemaBasedGenericDevice {
     private ServiceRegistration serviceRegistration;
 
     public CoAPDevice(URI uri) {
@@ -75,9 +77,9 @@ public class CoAPDevice extends SchemaBasedGenericDevice {
         System.out.println("[CoAPDevice]: " + "in notifyUpdate()");
         if (serviceRegistration != null) {
             System.out.println("[CoAPDevice]: " + "about to set property");
-            Properties props = this.getDeviceProperties();
-            props.setProperty(Constants.UPDATED_PATH, path);
-            serviceRegistration.setProperties(props);
+            Map<String, Object> props = this.getDeviceProperties();
+            props.put(Constants.UPDATED_PATH, path);
+            serviceRegistration.setProperties(LegacyUtil.toDictionary(props));
         }
 
     }
@@ -97,12 +99,11 @@ public class CoAPDevice extends SchemaBasedGenericDevice {
         System.out.println("[CoAPDevice]: in deviceUpdate()");
         // this code assumes that the stuff we want is in the first element of the array
         //System.out.println("[CoAPDevice]: " + " getServiceNames() " + java.util.Arrays.toString(getServiceNames()));
-        String[] localService = getServiceNames();
-        GenericDeviceService service = getService(localService[0]);
+        GDService service = getServices().values().iterator().next();
 
         //System.out.println("[CoAPDevice]: " + " keys() " + java.util.Arrays.toString(service.getProperties().getNames()));
-        String[] key = service.getProperties().getNames();
-        service.getProperties().setStringValue(key[0], data);
+        String key = service.getProperties().getProperties().keySet().iterator().next();
+        service.getProperties().setStringValue(key, data);
         // trigger update
         notifyUpdate(service.getPath(true));
 
--- osgi/adaptor.coap/src/main/java/com/ericsson/deviceaccess/coap/CoAPDeviceAgent.java
+++ osgi/adaptor.coap/src/main/java/com/ericsson/deviceaccess/coap/CoAPDeviceAgent.java
@@ -1,6 +1,6 @@
 /*
  * Copyright Ericsson AB 2011-2014. All Rights Reserved.
- * 
+ *
  * The contents of this file are subject to the Lesser GNU Public License,
  *  (the "License"), either version 2.1 of the License, or
  * (at your option) any later version.; you may not use this file except in
@@ -9,12 +9,12 @@
  * retrieved online at https://www.gnu.org/licenses/lgpl.html. Moreover
  * it could also be requested from Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * 
+ *
  * BECAUSE THE LIBRARY IS LICENSED FREE OF CHARGE, THERE IS NO
  * WARRANTY FOR THE LIBRARY, TO THE EXTENT PERMITTED BY APPLICABLE LAW.
  * EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR
  * OTHER PARTIES PROVIDE THE LIBRARY "AS IS" WITHOUT WARRANTY OF ANY KIND,
- 
+
  * EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
  * PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE
@@ -29,20 +29,12 @@
  * (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING RENDERED
  * INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A FAILURE
  * OF THE LIBRARY TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF SUCH
- * HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES. 
- * 
+ * HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ *
  */
 package com.ericsson.deviceaccess.coap;
 
-
-/**
- * CoAPDeviceAgent
- *
- * This class is instantiated by the CoAPDeviceFactory when a new CoAP device is discovered. The class creates 
- * a HashMap containing the relevant Service Schema (in this case, we use the WeatherResource and HelloWorld 
- * schema) in the update() method and also creates and registers a GenericDeviceImpl object with the OSGi framework.
- */
-
+import com.ericsson.commonutil.LegacyUtil;
 import com.ericsson.deviceaccess.api.GenericDevice;
 import com.ericsson.deviceaccess.coap.basedriver.api.CoAPException;
 import com.ericsson.deviceaccess.coap.basedriver.api.CoAPRemoteEndpoint;
@@ -51,20 +43,28 @@ import com.ericsson.deviceaccess.coap.basedriver.api.message.CoAPMessage;
 import com.ericsson.deviceaccess.coap.basedriver.api.message.CoAPRequest;
 import com.ericsson.deviceaccess.coap.basedriver.api.message.CoAPRequestListener;
 import com.ericsson.deviceaccess.coap.basedriver.api.message.CoAPResponse;
-import com.ericsson.deviceaccess.coap.basedriver.api.resources.CoAPResource;
 import com.ericsson.deviceaccess.coap.basedriver.api.resources.CoAPObservationResource;
+import com.ericsson.deviceaccess.coap.basedriver.api.resources.CoAPResource;
 import com.ericsson.deviceaccess.coap.basedriver.api.resources.CoAPResourceObserver;
-//import com.ericsson.research.ag.util.LogTracker;
+import java.net.URI;
+import java.util.Iterator;
+import java.util.Map.Entry;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 
-import java.net.URI;
-import java.util.Iterator;
-
-
+/**
+ * CoAPDeviceAgent
+ *
+ * This class is instantiated by the CoAPDeviceFactory when a new CoAP device is
+ * discovered. The class creates a HashMap containing the relevant Service
+ * Schema (in this case, we use the WeatherResource and HelloWorld schema) in
+ * the update() method and also creates and registers a GenericDeviceImpl object
+ * with the OSGi framework.
+ */
 public class CoAPDeviceAgent implements CoAPResourceObserver {
-    private BundleContext context;
+
+    private final BundleContext context;
     //private LogTracker log;
     private ServiceRegistration devReg;
     private CoAPService coapService;
@@ -77,110 +77,61 @@ public class CoAPDeviceAgent implements CoAPResourceObserver {
         //this.log = log;
 
         // Create an iterator to iterate through the resources a device might have attached to it
-        Iterator it = endpoint.getResources().keySet().iterator();
-        while (it.hasNext()) {
-            URI uri = (URI) it.next();
-            CoAPResource resource = (CoAPResource) endpoint.getResources().get(uri);
+        Iterator<Entry<URI, CoAPResource>> it = endpoint.getResources().entrySet().iterator();
+        it.forEachRemaining(entry -> {
+            URI uri = entry.getKey();
+            CoAPResource rsc = entry.getValue();
 
             // Check for known resources. This is one of the attributes of the GDA:
             // The matching between the device's resources and the service schemas
             // is currently statically defined.
-            if (resource.getUri().getPath().equals("/weatherResource")) {
-                System.out.println("[CoAPDeviceAgent]: FOUND WEATHER RESOURCE!");
-                // When a known resource is detected, create a CoAPDevice and attach the resource
-                coap = new CoAPDevice(uri);
-                coap.putService(new WeatherResourceImpl(coap));
+            switch (rsc.getUri().getPath()) {
+                case "/weatherResource":
+                    System.out.println("[CoAPDeviceAgent]: FOUND WEATHER RESOURCE!");
+                    // When a known resource is detected, create a CoAPDevice and attach the resource
+                    coap = new CoAPDevice(uri);
+                    coap.putService(new WeatherResourceImpl(coap));
+                    // Interface with basedriver to retrieve data from the resource
+                    try {
+                        ServiceReference<CoAPService> reference
+                                = context.getServiceReference(CoAPService.class);
+                        if (reference != null) {
+                            coapService = context.getService(reference);
+                        } else {
+                            //this.log
+                            //      .warn("Could still not fetch a reference to CoAPService");
+                        }
 
-                // Interface with basedriver to retrieve data from the resource
-                CoAPRequest req = null;
-                try {
-
-                    ServiceReference reference = context
-                            .getServiceReference(CoAPService.class.getName());
-                    if (reference != null) {
-                        coapService = (CoAPService) context
-                                .getService(reference);
-                    } else {
-                        //this.log
-                          //      .warn("Could still not fetch a reference to CoAPService");
+                        // Extract the details of the resource from the CoAPResource object
+                        path = rsc.getUri().getPath();
+                        CoAPRequest req = coapService.createGetRequest(rsc.getUri()
+                                .getHost(), rsc.getUri().getPort(),
+                                path, CoAPMessage.CoAPMessageType.CONFIRMABLE);
+                        // Create inner classes to handle possible responses to CoAP request
+                        req.setListener(new CoAPAgentListener());
+                        // Below commented out as CoAP can only handle one request at a time
+                        // This is how CoAP handles QoS
+                        // System.out.println("[CoAPDeviceAgent]: sent message!");
+                        // coapService.sendRequest(req);
+                    } catch (CoAPException e) {
+                        e.printStackTrace();
                     }
-
-                    // Extract the details of the resource from the CoAPResource object
-                    path = resource.getUri().getPath();
-                    req = coapService.createGetRequest(resource.getUri()
-                            .getHost().toString(), resource.getUri().getPort(),
-                            path, CoAPMessage.CoAPMessageType.CONFIRMABLE);
-                    // Create inner classes to handle possible responses to CoAP request
-                    req.setListener(new CoAPRequestListener() {
-                        public void resetResponseReceived(CoAPResponse resp, CoAPRequest req) {
-                             //TODO
-                        }
-
-                        public void emptyAckReceived(CoAPResponse response,
-                                                     CoAPRequest request) {
-                            // TODO Auto-generated method stub
-
-                        }
-
-                        public void maximumRetransmissionsReached(
-                                CoAPRequest request) {
-                            // TODO Auto-generated method stub
-
-                        }
-
-                        public void piggyPackedResponseReceived(
-                                CoAPResponse response, CoAPRequest request) {
-                            String payload = new String(response.getPayload());
-                            // System.out.println("[CoAPDeviceAgent]: Piggypacked message payload: ["
-                            // 		 + payload + "]");
-
-                        }
-
-                        public void requestTimeout(CoAPResponse response,
-                                                   CoAPRequest request) {
-                            // TODO Auto-generated method stub
-
-                        }
-
-                        public void separateResponseReceived(
-                                CoAPResponse response, CoAPRequest request) {
-                            String payload = new String(response.getPayload());
-                            // System.out.println("[CoAPDeviceAgent]: Separate message payload: ["
-                            // 		 + payload + "]");
-                            coap.deviceUpdate(payload);
-
-                        }
-
-                        public void serviceBusy(CoAPRequest request) {
-                            // TODO Auto-generated method stub
-
-                        }
-
-
-                    });
-                    // Below commented out as CoAP can only handle one request at a time
-                    // This is how CoAP handles QoS
-                    // System.out.println("[CoAPDeviceAgent]: sent message!");
-                    // coapService.sendRequest(req);
-                } catch (CoAPException e) {
-                    e.printStackTrace();
-                }
-
-            } else if (resource.getUri().getPath().equals("/helloWorld")) {
-                // HelloWorld resource is how we test CoAP Observations (subscription/notification)
-                System.out.println("[CoAPDeviceAgent]: FOUND HELLOWORLD RESOURCE!");
-                // can we move the below statement outside of this if/else clause?
-                // When a known resource is detected, create a CoAPDevice and attach the resource
-                coap = new CoAPDevice(uri);
-                coap.putService(new HelloWorldImpl(coap));
-                try {
-                    createObservationRelationShip();
-                } catch (CoAPException e) {
-                    System.out.println("an exception has occurred");
-                }
+                    break;
+                case "/helloWorld":
+                    // HelloWorld resource is how we test CoAP Observations (subscription/notification)
+                    System.out.println("[CoAPDeviceAgent]: FOUND HELLOWORLD RESOURCE!");
+                    // can we move the below statement outside of this if/else clause?
+                    // When a known resource is detected, create a CoAPDevice and attach the resource
+                    coap = new CoAPDevice(uri);
+                    coap.putService(new HelloWorldImpl(coap));
+                    try {
+                        createObservationRelationShip();
+                    } catch (CoAPException e) {
+                        System.out.println("an exception has occurred");
+                    }
+                    break;
             }
-
-        }
+        });
     }
 
     private void createObservationRelationShip() throws CoAPException {
@@ -197,7 +148,7 @@ public class CoAPDeviceAgent implements CoAPResourceObserver {
     }
 
     public void observeResponseReceived(CoAPResponse response,
-                                        CoAPResource resource) {
+            CoAPResource resource) {
         // Handle Observation responses
         System.out.println("***** OBSERVER RESPONSE RECEIVED *****");
         // if (logger != null) {
@@ -234,7 +185,7 @@ public class CoAPDeviceAgent implements CoAPResourceObserver {
         // Here we actually register the CoAPDevice object to the OSGi framework allowing the rest of the
         // GDA framework to interact with the CoAPDevice
         //log.info("[CoAPDeviceAgent]: Registering GenericDevImpl");
-        devReg = context.registerService(GenericDevice.class.getName(), coap, coap.getDeviceProperties());
+        devReg = context.registerService(GenericDevice.class, coap, LegacyUtil.toDictionary(coap.getDeviceProperties()));
         coap.setServiceRegistration(devReg);
     }
 
@@ -244,18 +195,70 @@ public class CoAPDeviceAgent implements CoAPResourceObserver {
 
     public void observeTerminationReceived(CoAPRequest arg0, String arg1) {
         // TODO Auto-generated method stub
-       // log.error("This method was missing, so just added a stub. ");
+        // log.error("This method was missing, so just added a stub. ");
     }
 
+    @Override
     public void observationRelationshipTerminated(CoAPResponse resp, CoAPObservationResource res, CoAPRequest req) {
         // TODO
     }
 
-    public void observeResponseReceived(CoAPResponse resp, CoAPResource res,CoAPRequest req) {
+    @Override
+    public void observeResponseReceived(CoAPResponse resp, CoAPResource res, CoAPRequest req) {
         //TODO
     }
 
     public void resetResponseReceived(CoAPResponse resp, CoAPRequest req) {
         //TODO
+    }
+
+    private class CoAPAgentListener implements CoAPRequestListener {
+
+        public CoAPAgentListener() {
+        }
+
+        @Override
+        public void resetResponseReceived(CoAPResponse resp, CoAPRequest req) {
+            //TODO
+        }
+
+        @Override
+        public void emptyAckReceived(CoAPResponse response,
+                CoAPRequest request) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void maximumRetransmissionsReached(
+                CoAPRequest request) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void piggyPackedResponseReceived(
+                CoAPResponse response, CoAPRequest request) {
+            String payload = new String(response.getPayload());
+            // System.out.println("[CoAPDeviceAgent]: Piggypacked message payload: ["
+            // 		 + payload + "]");
+
+        }
+
+        @Override
+        public void separateResponseReceived(
+                CoAPResponse response, CoAPRequest request) {
+            String payload = new String(response.getPayload());
+            // System.out.println("[CoAPDeviceAgent]: Separate message payload: ["
+            // 		 + payload + "]");
+            coap.deviceUpdate(payload);
+
+        }
+
+        @Override
+        public void serviceBusy(CoAPRequest request) {
+            // TODO Auto-generated method stub
+
+        }
     }
 }
